cb2937d7d837798309d432546ee5b70282625e0a
Makhnev Konstantin <mah.kons@gmail.com>
2021-02-23 15:59:31 +0300

    removig legacy controllers

--- configs/TimetableConfigs.py
+++ configs/TimetableConfigs.py
@@ -1,7 +1,6 @@
 from configs.Config import Config
 
 import torch
-from env.timetables import AllAgentLauncher, ShortestPathAgentLauncher, NetworkLoadAgentLauncher
 from agent.judge.Judge import Judge
 
 class TimeTableConfig(Config):
@@ -10,26 +9,6 @@ class TimeTableConfig(Config):
 
     def create_timetable(self):
         pass
-
-
-class AllAgentLauncherConfig(TimeTableConfig):
-    def create_timetable(self):
-        return AllAgentLauncher()
-
-
-class ShortestPathAgentLauncherConfig(TimeTableConfig):
-    def __init__(self, window_size_generator):
-        self.window_size_generator = window_size_generator
-
-    def create_timetable(self):
-        return ShortestPathAgentLauncher(self.window_size_generator)
-
-class NetworkLoadAgentLauncherConfig(TimeTableConfig):
-    def __init__(self, window_size_generator):
-        self.window_size_generator = window_size_generator
-
-    def create_timetable(self):
-        return NetworkLoadAgentLauncher(self.window_size_generator)
 
 class JudgeConfig(TimeTableConfig):
     def __init__(self, window_size_generator):
--- configs/__init__.py
+++ configs/__init__.py
@@ -1,10 +1,9 @@
 from .Experiment import Experiment
 
-from .MemoryConfigs import ReplayMemoryConfig
 from .OptimizerConfigs import AdamConfig
-from .ControllerConfigs import ActorCriticConfig, DQNConfig, PPOConfig
+from .ControllerConfigs import PPOConfig
 from .EnvConfigs import FlatlandConfig, CartPoleConfig, LunarLanderConfig, EnvCurriculumConfig, EnvCurriculumSampleConfig
 from .ObsBuilderConfigs import SimpleObservationConfig, ShortPathObsConfig
 from .RewardConfigs import SimpleRewardConfig, SparseRewardConfig, NearRewardConfig, \
         DeadlockPunishmentConfig, RewardsComposerConfig, NotStopShaperConfig, FinishRewardConfig
-from .TimetableConfigs import AllAgentLauncherConfig, ShortestPathAgentLauncherConfig, NetworkLoadAgentLauncherConfig, JudgeConfig
+from .TimetableConfigs import JudgeConfig
